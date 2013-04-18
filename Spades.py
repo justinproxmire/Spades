9c2534aa0f7d50db8e64447d8c757cd431ac8145
Justin Proxmire <justin.proxmire@gmail.com>
2013-04-17 22:03:19 -0400

    - added git ignore

---
+++ Spades.py
@@ -0,0 +1,12 @@
+# PROGRAM:				Spades - Spades.py
+# AUTHORS:				
+# DATE:						4/17/13
+# DESCRIPTION:		
+# MODULES:		
+#			
+#			
+#	
+# NOTES:					CS5300 - Final Project 2013
+# ***********************************************
+
+if __name__ == "__main__":
---
+++ SpadesTask.py
@@ -0,0 +1,10 @@
+# PROGRAM:				Spades - SpadesTask.py
+# AUTHORS:				
+# DATE:						4/17/13
+# DESCRIPTION:		
+# MODULES:		
+#			
+#			
+#	
+# NOTES:					CS5300 - Final Project 2013
+# ***********************************************
