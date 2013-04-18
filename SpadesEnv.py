abca5dd79052cb4519d6f4f3db97fbeb5fb5f10c
Justin Hill <jhill.d@gmail.com>
2013-04-17 22:05:59 -0400

    - Added header to environment file

---
+++ SpadesEnv.py
@@ -0,0 +1,11 @@
+# PROGRAM:				Spades - SpadesEnv.py
+# AUTHORS:				
+# DATE:						4/17/13
+# DESCRIPTION:		Spades environment class.  Don't really know what this does yet.
+# MODULES:		
+#			
+#			
+#	
+# NOTES:					CS5300 - Final Project 2013
+# ***********************************************
+
