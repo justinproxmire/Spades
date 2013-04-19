ca11066993d81ef42b5f6fbd5e2f13431a66b7aa
Justin Hill <jhill.d@gmail.com>
2013-04-19 18:59:06 -0400

    - Filled out the SpadesEnv class a bit.

--- SpadesEnv.py
+++ SpadesEnv.py
@@ -10,14 +10,41 @@
 # ***********************************************
 
 from pybrain.rl.environments.environment import Environment
+import numpy as np
 import SpadesPlayer
+import SpadesDeck
 
 class SpadesEnv(Environment):
 
+    # the number of action values the environment accepts
+    indim = 4
+
+    # the number of sensor values the environment produces
+    outdim = 1
+
+    # discrete state space
+    discreteStates = True
+
+    # discrete action space
+    discreteActions = True
+
+
 	def __init__(self, game_deck):
 		self.game_deck = game_deck
-		self.in_dim = None
-		self.out_dim = None
 
-	
+	def getSensors(self):
+		"""
+			Get the internal state of the environment.  Returns a numpy array of doubles.
+		"""
+		return np.array()
 
+	def performAction(self, action):
+		"""
+			Action is a numpy array of doubles.  No return value.
+		"""
+
+	def reset(self):
+		"""
+			Resets the environment's internal state.  No return value.
+		"""
+		self.game_deck = SpadesDeck()
